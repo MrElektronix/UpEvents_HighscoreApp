3395273b700569cbb556e27d4c2278bc08f51820
noahsenteur <noahsenteur12345@gmail.com>
2018-03-29 10:39:31 +0200

    Update

---
+++ Assets/FrontEndApp/Scripts/DeviceCamera/TakePhoto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class TakePhoto : MonoBehaviour {
+
+	WebCamTexture _webCamTexture;
+	public string path = "";
+	public string filename = "Snapshots";
+
+	void Start(){
+		_webCamTexture = new WebCamTexture ();
+	}
+
+
+	public void OnClick(){
+		TakeSnapShot ();
+	}
+
+
+
+	void TakeSnapShot(){
+		/*
+		Texture2D snap = new Texture2D (_webCamTexture.width, _webCamTexture.height);
+		snap.SetPixels (_webCamTexture.GetPixels(1, 1,_webCamTexture.height,_webCamTexture.width));
+		snap.Apply ();
+		byte[] bytes = snap.EncodeToPNG ();
+		path = Application.persistentDataPath 
+		*/
+		ScreenCapture.CaptureScreenshot(filename);
+
+
+	}
+}
