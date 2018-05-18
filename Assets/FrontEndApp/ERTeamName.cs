b7ff65c63574f862f5aa939399a45fc02cdeea96
noahsenteur <noahsenteur12345@gmail.com>
2018-05-18 12:05:07 +0200

    Team

--- Assets/FrontEndApp/ERTeamName.cs
+++ Assets/FrontEndApp/ERTeamName.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class ERTeamName : MonoBehaviour {
 
 	public InputField TeamNameInput;
 	private string _tNameDataEscapeRoom;
-
+	public GameObject button;
 
 	void Start(){
 		TeamNameInput = gameObject.GetComponent<InputField> ();
-
+		button.SetActive (false);
 	}
-
 	void Update(){
 		_tNameDataEscapeRoom = TeamNameInput.text;
 		PlayerPrefs.SetString ("EscapeRoomTeamName", _tNameDataEscapeRoom);
+		if (TeamNameInput.text == "") {
+			button.SetActive (false);
+		} else {
+			button.SetActive (true);
+		}
 	}
-
-
-
 }
