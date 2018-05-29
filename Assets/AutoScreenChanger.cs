3c7a826a6ca2fd4d30efb7724990ce82fe264bbd
noahsenteur <noahsenteur12345@gmail.com>
2018-05-29 11:17:16 +0200

    Eind Scherm
    Eind scherm met bedankt die automatisch terug gaat naar het hoofdscherm

---
+++ Assets/AutoScreenChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AutoScreenChanger : MonoBehaviour {
+
+	public int scene;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (ExecuteOverTime ());
+	}
+	
+	IEnumerator ExecuteOverTime (){
+		yield return new WaitForSeconds (5);
+
+		SceneManager.LoadScene (scene);
+	}
+}
